7898764a22cd9fc933ff4ef4c76baf271b8c522e
purupurupu <purupurupu0prog@gmail.com>
2023-01-20 15:02:21 +0900

    some setting complete

---
+++ problems/python/1.two-sum.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # @lc code=end
