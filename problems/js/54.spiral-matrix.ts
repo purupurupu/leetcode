438a546a198f4011fedfdfd7e1ecda55484ee617
purupurupu <purupurupu0prog@gmail.com>
2023-02-15 23:11:48 +0900

    solved by ts

--- problems/js/54.spiral-matrix.ts
+++ problems/js/54.spiral-matrix.ts
@@ -1,3 +1,41 @@
 function spiralOrder(matrix: number[][]): number[] {
-  return [0];
+  let arr: number[] = [];
+  let r: number = matrix.length;
+  let c: number = matrix[0].length;
+  let direction: number = 0;
+  let left: number = 0;
+  let right: number = c - 1;
+  let bottom: number = 0;
+  let top: number = r - 1;
+
+  while (arr.length < r * c) {
+    if (direction == 0) {
+      for (let i = left; i <= right; i++) {
+        arr.push(matrix[bottom][i]);
+      }
+      bottom++;
+      direction++;
+    } else if (direction == 1) {
+      for (let i = bottom; i <= top; i++) {
+        arr.push(matrix[i][right]);
+      }
+      right--;
+      direction++;
+    } else if (direction == 2) {
+      for (let i = right; i >= left; i--) {
+        arr.push(matrix[top][i]);
+      }
+      top--;
+      direction++;
+    } else if (direction == 3) {
+      for (let i = top; i >= bottom; i--) {
+        arr.push(matrix[i][left]);
+      }
+      left++;
+      direction++;
+    }
+    if (direction == 4) direction = 0;
+  }
+
+  return arr;
 }
