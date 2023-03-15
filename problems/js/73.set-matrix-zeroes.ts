c3746b4e112c1161bdd84f1c093f00fdfe9acddb
purupurupu <purupurupu0prog@gmail.com>
2023-03-15 22:27:57 +0900

    no.73 solved.

---
+++ problems/js/73.set-matrix-zeroes.ts
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=73 lang=typescript
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const rows = new Set();
+  const cols = new Set();
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (rows.has(i) || cols.has(j)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+}
+
+const matrix = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+];
+
+const res = setZeroes(matrix);
+console.log(res);
+
+// @lc code=end
