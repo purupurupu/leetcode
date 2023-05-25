065c8e391498c7bb07cddbdf050ad7515f169c09
purupurupu <purupurupu0prog@gmail.com>
2023-05-25 23:31:57 +0900

    228 faster

--- problems/js/228.summary-ranges.ts
+++ problems/js/228.summary-ranges.ts
@@ -5,17 +5,31 @@
  */
 
 // @lc code=start
+// function summaryRanges(nums: number[]): string[] {
+//   let res: string[] = [];
+//   let tmp = nums[0];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i + 1] - nums[i] === 1) {
+//       continue;
+//     } else {
+//       res.push(tmp === nums[i] ? `${tmp}` : `${tmp}->${nums[i]}`);
+//       tmp = nums[i + 1];
+//     }
+//   }
+//   return res;
+// }
+
+// faster case
 function summaryRanges(nums: number[]): string[] {
   let res: string[] = [];
-  let tmp = nums[0];
+  let start = nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i + 1] - nums[i] === 1) {
-      continue;
-    } else {
-      res.push(tmp === nums[i] ? `${tmp}` : `${tmp}->${nums[i]}`);
-      tmp = nums[i + 1];
-    }
+  for (let i = 1; i <= nums.length; i++) {
+    if (nums[i] - nums[i - 1] === 1) continue;
+
+    res.push(start === nums[i - 1] ? `${start}` : `${start}->${nums[i - 1]}`);
+    start = nums[i];
   }
   return res;
 }
---
+++ problems/js/tempCodeRunnerFile.ts
@@ -0,0 +1,10 @@
+  let res: string[] = [];
+  let start = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] === 1) continue;
+
+    res.push(start === nums[i - 1] ? `${start}` : `${start}->${nums[i - 1]}`);
+    start = nums[i];
+  }
+  return res;
