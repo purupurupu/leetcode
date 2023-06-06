fdee074e564be077e8ea39b9b8fc5b376fd36df3
purupurupu <purupurupu0prog@gmail.com>
2023-06-06 21:20:18 +0900

    No.167 two pointers solution

--- problems/js/167.two-sum-ii-input-array-is-sorted.ts
+++ problems/js/167.two-sum-ii-input-array-is-sorted.ts
@@ -5,16 +5,36 @@
  */
 
 // @lc code=start
+// function twoSum(numbers: number[], target: number): number[] {
+//   let res: number[] = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i; j < numbers.length; j++) {
+//       if (i == j) continue;
+//       if (numbers[i] + numbers[j] === target) {
+//         res.push(i + 1);
+//         res.push(j + 1);
+//         return res;
+//       }
+//     }
+//   }
+//   return res;
+// }
+
+// two pointers solution
 function twoSum(numbers: number[], target: number): number[] {
   let res: number[] = [];
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i; j < numbers.length; j++) {
-      if (i == j) continue;
-      if (numbers[i] + numbers[j] === target) {
-        res.push(i + 1);
-        res.push(j + 1);
-        return res;
-      }
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      res.push(left + 1);
+      res.push(right + 1);
+      break;
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
     }
   }
   return res;
