244986f966a2e5936118159884959a31f4920964
purupurupu <purupurupu0prog@gmail.com>
2023-06-06 21:16:16 +0900

    No.167 solved

---
+++ problems/js/167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=167 lang=typescript
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+function twoSum(numbers: number[], target: number): number[] {
+  let res: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i; j < numbers.length; j++) {
+      if (i == j) continue;
+      if (numbers[i] + numbers[j] === target) {
+        res.push(i + 1);
+        res.push(j + 1);
+        return res;
+      }
+    }
+  }
+  return res;
+}
+
+let numbers = [2, 7, 11, 15];
+let target = 9;
+console.log(twoSum(numbers, target));
+
+// @lc code=end
