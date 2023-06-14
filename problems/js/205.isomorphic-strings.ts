09e689079ebfa03e4822c7cf25ba6d2bba88760d
purupurupu <purupurupu0prog@gmail.com>
2023-06-14 21:56:34 +0900

    No.205 resolve faster

--- problems/js/205.isomorphic-strings.ts
+++ problems/js/205.isomorphic-strings.ts
@@ -5,26 +5,37 @@
  */
 
 // @lc code=start
+// function isIsomorphic(s: string, t: string): boolean {
+//   if (s.length !== t.length) return false;
+//   let map = new Map();
+//   for (let i = 0; i < s.length; i++) {
+//     if (!map.has(s[i])) {
+//       map.set(s[i], t[i]);
+//     } else {
+//       if (map.get(s[i]) !== t[i]) return false;
+//     }
+//   }
+
+//   map = new Map();
+//   for (let i = 0; i < t.length; i++) {
+//     if (!map.has(t[i])) {
+//       map.set(t[i], s[i]);
+//     } else {
+//       if (map.get(t[i]) !== s[i]) return false;
+//     }
+//   }
+//   return true;
+// }
+
+// faster
 function isIsomorphic(s: string, t: string): boolean {
   if (s.length !== t.length) return false;
-  let map = new Map();
+
   for (let i = 0; i < s.length; i++) {
-    if (!map.has(s[i])) {
-      map.set(s[i], t[i]);
-    } else {
-      if (map.get(s[i]) !== t[i]) return false;
+    if (s.indexOf(s[i]) !== t.indexOf(t[i])) {
+      return false;
     }
   }
-
-  map = new Map();
-  for (let i = 0; i < t.length; i++) {
-    if (!map.has(t[i])) {
-      map.set(t[i], s[i]);
-    } else {
-      if (map.get(t[i]) !== s[i]) return false;
-    }
-  }
-
   return true;
 }
 let s = "egg",
