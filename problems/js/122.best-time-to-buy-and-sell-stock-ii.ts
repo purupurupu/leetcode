ed307e71dff698ddb70dc9421c7d590ee9a10039
purupurupu <purupurupu0prog@gmail.com>
2023-06-14 21:04:25 +0900

    No.122 resolve

---
+++ problems/js/122.best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=122 lang=typescript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  let left = 0;
+  let right = prices.length - 1;
+  let profit = 0;
+  while (left < right) {
+    if (prices[left] < prices[left + 1]) {
+      profit += prices[left + 1] - prices[left];
+    }
+    left++;
+  }
+  return profit;
+}
+
+let prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
+
+// @lc code=end
