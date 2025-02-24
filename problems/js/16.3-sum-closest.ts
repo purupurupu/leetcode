69392f7c0da50818076d5f06fb2d0559b3756e89
purupurupu <purupurupu0prog@gmail.com>
2025-02-24 22:56:58 +0900

    No.16 two pointers case

--- problems/js/16.3-sum-closest.ts
+++ problems/js/16.3-sum-closest.ts
@@ -5,27 +5,58 @@
  */
 
 // @lc code=start
+// function threeSumClosest(nums: number[], target: number): number {
+
+//     // init
+//     let sum = nums[0] + nums[1] + nums[2]
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             for (let k = j + 1; k < nums.length; k++) {
+//                 const currentSum = nums[i] + nums[j] + nums[k]
+//                 if (currentSum === target) return currentSum
+//                 if (Math.abs(currentSum - target) < Math.abs(sum - target)) {
+//                     sum = currentSum
+//                 }
+//             }
+//         }
+//     }
+
+//     return sum
+// };
+
 function threeSumClosest(nums: number[], target: number): number {
 
-    // init
-    let sum = nums[0] + nums[1] + nums[2]
+    const n = nums.length
+    const sortedNums = nums.sort((a, b) => a - b)
+    let ans = sortedNums[0] + sortedNums[1] + sortedNums[2]
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                const currentSum = nums[i] + nums[j] + nums[k]
-                if (currentSum === target) return currentSum
-                if (Math.abs(currentSum - target) < Math.abs(sum - target)) {
-                    sum = currentSum
-                }
+    for (let i = 0; i < n; i++) {
+        let left = i + 1
+        let right = n - 1
+
+        while (left < right) {
+            const currentSum = sortedNums[i] + sortedNums[left] + sortedNums[right]
+            const diff = Math.abs(currentSum - target)
+
+            if (diff < Math.abs(ans - target)) {
+                ans = currentSum
             }
-        }
+
+            if (currentSum < target) {
+                left++
+            } else if (currentSum > target) {
+                right--
+            } else {
+                return currentSum
+            }
+        }            
     }
 
-    return sum
+    return ans
 };
 
 console.log(threeSumClosest([-1, 2, 1, -4], 1))
-console.log(threeSumClosest([0, 0, 0], 1))
+// console.log(threeSumClosest([0, 0, 0], 1))
 // @lc code=end
 
